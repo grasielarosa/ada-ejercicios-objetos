62dd2197a4b6c68986aef22432eb5b55e1d0fcce
grasielarosa <grasielarosaa@gmail.com>
2021-07-23 12:00:43 +0200

    funciones con objetos - ejercicio 1

---
+++ funciones-con-objetos/script.js
@@ -0,0 +1,19 @@
+// * Ejercicio 1: Nombre completo
+/*
+let persona = {
+    nombre: 'Grace',
+    apellido: 'Hopper',
+    email: 'grace.hopper@gmail.com',
+    plan: 'premium'
+  };
+
+  const obtenerNombreCompleto = (persona) => {
+    const { nombre, apellido } = persona;
+    return`${nombre} ${apellido}`
+  }
+  console.log(obtenerNombreCompleto(persona));
+*/
+
+
+
+  
